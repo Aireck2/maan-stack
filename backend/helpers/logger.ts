6fc46b11565d9a1cd7e583f4410ac2c3ff821d51
@erick <erick.hx22@gmail.com>
2022-09-14 09:47:50 -0500

    chore: add config/logger/utils helpers (#1)

---
+++ backend/helpers/logger.ts
@@ -0,0 +1,39 @@
+import { createLogger, format, transports } from "winston";
+
+const logLevels = {
+  fatal: 0,
+  error: 1,
+  warn: 2,
+  info: 3,
+  debug: 4,
+  trace: 5,
+};
+
+const logFormat = format.combine(
+  format.timestamp({ format: "DD.MM.YYYY HH:mm:ss a" }),
+  format.prettyPrint(),
+  format.printf(
+    (content) =>
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      `[${content.timestamp}] - [${content.level.toUpperCase()}] [${
+        content.context
+      }] - ${content.message}`
+  )
+);
+
+const logger = createLogger({
+  levels: logLevels,
+  format: logFormat,
+  defaultMeta: {
+    service: "process",
+  },
+  transports: [new transports.Console()],
+  exceptionHandlers: [
+    new transports.File({ filename: `${process.cwd()}/exceptions.log` }),
+  ],
+  rejectionHandlers: [
+    new transports.File({ filename: `${process.cwd()}/rejections.log` }),
+  ],
+});
+
+export default logger;
