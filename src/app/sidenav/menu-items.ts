41283ba7a2924e7e2bfa304ecbfcb7b624ec28a1
@erick <erick.hx22@gmail.com>
2022-09-14 18:55:23 -0500

    chore: add initial angular app setup (#4)

---
+++ src/app/sidenav/menu-items.ts
@@ -0,0 +1,12 @@
+export const menuItems = [
+  {
+    key: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Dashboard',
+  },
+  {
+    key: 'product',
+    icon: 'fal fa-home',
+    label: 'Products',
+  },
+];
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { menuItems } from './menu-items';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss'],
+})
+export class SidenavComponent implements OnInit {
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+  collapsed = true;
+  screenWidth = 0;
+  navData = menuItems;
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
