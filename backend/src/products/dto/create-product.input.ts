9470c300451914bc65ac6debf4064e40b4399ff5
@erick <erick.hx22@gmail.com>
2022-09-16 02:00:18 -0500

    chore: add input get/upd product (#8)

--- backend/src/products/dto/create-product.input.ts
+++ backend/src/products/dto/create-product.input.ts
@@ -1,4 +1,5 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, ID, InputType } from '@nestjs/graphql';
+import { Schema } from 'mongoose';
 @InputType()
 export class InputCategory {
   @Field()
@@ -26,4 +27,27 @@ export class InputAddProduct {
   slug: string;
   @Field()
   status: string;
+}
+
+@InputType()
+export class InputUpdProduct {
+  @Field()
+  _id: string;
+  @Field({ nullable: true })
+  name: string;
+  @Field({ nullable: true })
+  category: InputCategory;
+  @Field({ nullable: true })
+  brand: InputBrand;
+  @Field({ nullable: true })
+  slug: string;
+  @Field({ nullable: true })
+  status: string;
+  @Field({ nullable: true })
+  isVisible: boolean;
+}
+@InputType()
+export class InputGetProduct {
+  @Field()
+  _id: string;
 }
