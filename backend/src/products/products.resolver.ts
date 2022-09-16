6ce732800ee5b3e6a16ba5409aeced61068a8550
@erick <erick.hx22@gmail.com>
2022-09-16 02:02:11 -0500

    chore: add get/upd product resolver (#8)

--- backend/src/products/products.resolver.ts
+++ backend/src/products/products.resolver.ts
@@ -3,8 +3,13 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PaginatedProduct, Product } from './product.entity';
 import { ProductsService } from './products.service';
 
-import { InputAddProduct } from './dto/create-product.input';
+import {
+  InputAddProduct,
+  InputUpdProduct,
+  InputGetProduct,
+} from './dto/create-product.input';
 import { Paginated, FilterProduct } from './dto/get-product.input';
+import { NotFoundException } from '@nestjs/common';
 
 @Resolver(() => Product)
 export class ProductsResolver {
@@ -19,10 +24,24 @@ export class ProductsResolver {
   ): Promise<PaginatedProduct> {
     return this.productsService.findAll(paginated, filter);
   }
+  @Query(() => Product)
+  getProduct(@Args('input') input: InputGetProduct) {
+    const product = this.productsService.obtain(input);
+    return product;
+  }
 
   @Mutation(() => Product)
   addProduct(@Args('input') input: InputAddProduct): Promise<Product> {
     const newProduct = this.productsService.create(input);
     return newProduct;
+  }
+  @Mutation(() => Product)
+  updProduct(@Args('input') input: InputUpdProduct) {
+    const product = this.productsService.obtain(input);
+    if (!product) {
+      throw new NotFoundException(`Order ${input._id} not found`);
+    }
+    const editProduct = this.productsService.update(input);
+    return editProduct;
   }
 }
