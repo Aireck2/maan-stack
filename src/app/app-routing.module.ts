46e2d5422c8551384602551361fb75732bd02750
@erick <erick.hx22@gmail.com>
2022-09-18 14:13:22 -0500

    refactor: remove unnecessary code (#10)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
-import { ProductsComponent } from './products/products.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+import { ProductsTableComponent } from './components/products-table/products-table.component';
+
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'products', component: ProductsComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductsTableComponent },
 ];
 
 @NgModule({
