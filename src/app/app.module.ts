ec8ebfd4e1ce446027e0e1aa83e6e2331f3db052
@erick <erick.hx22@gmail.com>
2022-09-18 14:15:13 -0500

    chore: add  graphql module to app.imports (#10)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BodyComponent } from './body/body.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GraphqlModule } from './services/graphql.module';
 
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { ProductsTableComponent } from './components/products-table/products-table.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BodyComponent,
-    SidenavComponent,
-    // DashboardComponent,
-    // ProductsComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserAnimationsModule,
     AppRoutingModule,
+    GraphqlModule,
     CommonModule,
     ToolbarComponent,
     ProductsTableComponent,
